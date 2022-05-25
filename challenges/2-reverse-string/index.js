1a2363b2d5e867a99b9cf8134e974fe26da20d9d
fernando henrique <fernandomk6@gmail.com>
2022-05-25 19:02:22 -0300

    desafios concluidos

--- challenges/2-reverse-string/index.js
+++ challenges/2-reverse-string/index.js
@@ -5,6 +5,9 @@ const reverseString = (string) => {
 
   let newString = "";
 
+  // i = ultimo indice da string
+  // cada iteração o i é decrementado ate ficar = 0, que é o primeiro indice da string
+  // para cada iteração o caractere do indice i, é concatenado na newString
   for (let i = string.length - 1; i >= 0; i--) {
     newString += string[i];
   }
